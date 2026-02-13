4ea31fc8836454bc403abe78ff52f1dac0d2a9df
Mohammad Fallah <mohammadfallah.w@gmail.com>
2026-02-13 16:33:41 +0330

    feat: :sparkles: Implement protect route

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,3 +1,10 @@
+import { Navigate } from "react-router";
+import { useAuthStore } from "../stores";
+
 export const SignupPage = () => {
+  const user = useAuthStore((s) => s.user);
+
+  if (user) return <Navigate to="/" replace />;
+
   return <div>Signup</div>;
 };
